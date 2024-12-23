1abcc346e968c6b09352af6cefecdb028d543b37
Alex Steingart <alexsteingart@gmail.com>
2024-12-23 07:11:31 -0500

    empty google map on page

---
+++ app/ui/map/venues-map.tsx
@@ -0,0 +1,18 @@
+'use client';
+import {APIProvider, Map, MapCameraChangedEvent} from '@vis.gl/react-google-maps';
+
+export default function VenuesMap() {
+  return (
+    <APIProvider
+        apiKey={'AIzaSyCUWVZ7sLZmBd_rBXiSyFzn5cgUmKDYlQQ'}
+        onLoad={() => console.log('Maps API has loaded.')}>
+      <Map
+         defaultZoom={13}
+         defaultCenter={ { lat: 40.727094, lng: -73.946729 } }
+         onCameraChanged={ (ev: MapCameraChangedEvent) =>
+           console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
+         }>
+      </Map>
+    </APIProvider>
+  )
+}
